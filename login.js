62fc5e177b8a016689ece424e024a1a06ba1ecb5
franciscosucho <franciscosuchomela@gmail.com>
2024-08-19 18:41:59 -0300

    proyecto modularizado

---
+++ login.js
@@ -0,0 +1,214 @@
+const mysql = require('mysql2');
+const { ipcRenderer } = require('electron');
+
+
+
+// parte de los addlisterner de el form
+const btn_iniciar = document.getElementById("btn_iniciar");
+const btn_registrar = document.getElementById("btn_registrar");
+const cont_registrar = document.getElementById("cont_registrar");
+const cont_iniciar = document.getElementById("cont_iniciar");
+//uso de las depencias para hashear contras
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+
+//  registro
+btn_registrar.addEventListener("click", () => {
+    btn_registrar.classList.toggle("activo");
+    btn_iniciar.classList.toggle("activo");
+    cont_registrar.classList.toggle("desac");
+    cont_iniciar.classList.toggle("desac");
+});
+document.getElementById('btn_enviar_registro').addEventListener('click', async (event) => {
+    event.preventDefault();  // Evita que la página se recargue
+
+    const dni = document.getElementById('dni').value.trim();
+    const nombre_usuario = document.getElementById('nombre_usuario_regis').value.trim();
+    let password = document.getElementById('password_regis').value.trim();
+    let nombre_regis = document.getElementById('nombre_regis').value.trim();
+    let apellido_regis = document.getElementById('apellido_regis').value.trim();
+    const fechaNacimiento = document.getElementById('nacimiento').value.trim();
+    const email = document.getElementById('email_regis').value.trim();
+    const peso = document.getElementById('peso').value.trim();
+    const altura = document.getElementById('altura').value.trim();
+    const dieta = document.getElementById('dieta').value.trim();
+    const objetivo_nutricional = document.getElementById('objetivo_nutricional').value.trim();
+    const tipo_deporte = document.getElementById('tipo_deporte').value.trim();
+    const obj_deportivo = document.getElementById('obj_deportivo').value.trim();
+    const frecuencia = document.getElementById('frecuencia').value.trim();
+    const intensidad = document.getElementById('intensidad').value.trim();
+    const genero = document.getElementById('opciones_genero').value.trim();
+
+
+
+    // Uso en el proceso de registro
+    nombre_regis = capitalizeFirstLetter(nombre_regis);
+    apellido_regis = capitalizeFirstLetter(apellido_regis);
+    try {
+        password = await bcrypt.hash(password, saltRounds); // Espera el hash
+
+        if (dni && nombre_usuario && obj_deportivo && dieta && objetivo_nutricional && tipo_deporte && frecuencia && intensidad && password && apellido_regis && nombre_regis && fechaNacimiento && email && peso && altura && genero) {
+            const formData = {
+                dni,
+                nombre_usuario,
+                password,  // Contraseña hasheada
+                nombre_regis,
+                apellido_regis,
+                fechaNacimiento,
+                email,
+                peso,
+                altura,
+                genero,
+                objetivo_nutricional,
+                dieta,
+                obj_deportivo,
+                tipo_deporte,
+                frecuencia,
+                intensidad
+            };
+
+            // Enviar datos al proceso principal
+            ipcRenderer.send('submit-registration', formData);
+            document.getElementById("ventana_prin").classList.toggle("desac");
+            document.getElementById("registro").classList.toggle("desac");
+        } else {
+            alert("Por favor, complete todos los campos.");
+        }
+    } catch (error) {
+        console.error('Error hashing password:', error);
+    }
+});
+
+//logeo
+let nombre_ini 
+document.getElementById('btn_ini_sesion').addEventListener('click', async (event) => {
+    event.preventDefault();  // Evita que la página se recargue
+
+    // Datos del logeo
+     nombre_ini = document.getElementById('nombre_ini').value.trim();
+    const email_ini = document.getElementById('nombre_ini').value.trim();
+    const password_ini = document.getElementById('password_ini').value.trim();  // No hashees la contraseña aquí
+
+    if (nombre_ini && email_ini && password_ini) {
+        const data_ini = {
+            nombre_ini,
+            email_ini,
+            password_ini,  // Enviar la contraseña sin hashear
+        };
+
+        // Enviar datos al proceso principal
+        ipcRenderer.send('submit-ini', data_ini);
+    } else {
+        alert("Por favor, complete todos los campos.");
+    }
+});
+
+// Escuchar la respuesta del proceso principal
+ipcRenderer.on('login-response', (event, response) => {
+    if (response === 'success') {
+        // Si el logeo fue exitoso, cambiar la visibilidad de las ventanas
+        document.getElementById("ventana_prin").classList.toggle("desac");
+        document.getElementById("registro").classList.toggle("desac");
+        document.getElementById("nombre_us").textContent = nombre_ini;
+    } else {
+        alert(response);  // Mostrar un mensaje de error si la autenticación falla
+    }
+});
+
+// Inicializa el primer formulario como visible
+let formularioActual = 0;
+
+
+btn_iniciar.addEventListener("click", () => {
+    btn_registrar.classList.toggle("activo");
+    btn_iniciar.classList.toggle("activo");
+    cont_registrar.classList.toggle("desac");
+    cont_iniciar.classList.toggle("desac");
+})
+
+const texto_span = [
+    "Paso 1: Datos básicos.",
+    "Paso 2: Datos de acceso",
+    "Paso 3: Datos nutricionales",
+    "Paso 4: Datos deportivos"
+];
+const cont_registro = document.getElementById("registro");
+const contenedor = document.getElementById('cont_registrar');
+const subForms = contenedor.getElementsByClassName('sub_form');
+const botones = contenedor.getElementsByClassName('btn_enviar_sesion');
+const botones_atras = contenedor.getElementsByClassName('btn_atras');
+const texto_regs = document.getElementById("texto_regs");
+function btn_siguiente() {
+
+    if (formularioActual === 0) {
+        texto_regs.innerHTML = texto_span[formularioActual];
+        subForms[formularioActual].classList.remove("desac");
+    }
+
+    // añade el event listener cuando haga click
+    Array.from(botones).forEach((boton, indice) => {
+        boton.addEventListener('click', function (event) {
+            // Evita que el formulario se envíe y recargue la página
+            event.preventDefault();
+
+            if (formularioActual < subForms.length - 1) {
+                subForms[formularioActual].classList.add("desac");
+                formularioActual++;
+                subForms[formularioActual].classList.remove("desac");
+                texto_regs.innerHTML = texto_span[formularioActual];
+            } else if (formularioActual === subForms.length - 1) {
+                formularioActual++;
+                cont_registro.classList.add("desac");
+                // //Recolectar datos de cada formulario
+                // if (index === 0) {
+                //     formData.nombreCompleto = document.querySelector('#datos_personales input[type="text"]').value;
+                //     formData.dni = document.querySelector('#datos_personales input[type="number"]').value;
+                //     formData.fechaNacimiento = document.querySelector('#datos_personales input[type="date"]').value;
+                //     formData.genero = document.querySelector('#datos_personales select#opciones').value;
+                //     formData.peso = document.querySelector('#datos_personales input[placeholder="Escriba su peso en kgs"]').value;
+                //     formData.altura = document.querySelector('#datos_personales input#altura').value;
+                // } else if (index === 1) {
+                //     formData.nombreUsuario = document.querySelector('#datos_acceso input[type="text"]').value;
+                //     formData.email = document.querySelector('#datos_acceso input[type="email"]').value;
+                //     formData.password = document.querySelector('#datos_acceso input[type="password"]').value;
+                // } else if (index === 2) {
+                //     formData.tipoDieta = document.querySelector('#datos_nutricionales select#dieta').value;
+                //     formData.objetivoNutricional = document.querySelector('#datos_nutricionales select#objetivo_nutricional').value;
+                // } else if (index === 3) {
+                //     formData.deporte = document.querySelector('#datos_deportivos input[placeholder="Eliga su deportivo"]').value;
+                //     formData.objetivoDeportivo = document.querySelector('#datos_deportivos input[placeholder="Eliga su Objetivo deportivo"]').value;
+                //     formData.frecuencia = document.querySelector('#datos_deportivos input[placeholder="Frecuencia con la que realiza el deporte"]').value;
+                //     formData.intensidad = document.querySelector('#datos_deportivos input[placeholder="Intensidad con la que realiza el deporte"]').value;
+
+                //     // Al hacer clic en el último botón "Siguiente", enviar los datos al servidor
+
+                // }
+                // enviarDatosAlServidor(formData);
+
+            }
+        });
+    });
+    // añade el event listener cuando haga click
+    Array.from(botones_atras).forEach((boton, indice) => {
+        boton.addEventListener('click', function (event) {
+            // Evita que el formulario se envíe y recargue la página
+
+            event.preventDefault();
+
+            if (formularioActual < subForms.length && formularioActual > 0) {
+                subForms[formularioActual].classList.add("desac");
+                formularioActual--;
+                subForms[formularioActual].classList.remove("desac");
+                texto_regs.innerHTML = texto_span[formularioActual];
+            }
+        });
+    });
+}
+btn_siguiente();
--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
 const mysql = require('mysql2');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-function createWindow() {
-  const win = new BrowserWindow({
+const bcrypt = require('bcrypt');
+
+let loginWindow;
+let mainWindow;
+
+function createWindowLogin() {
+  loginWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,  // Permitir la integración de Node.js
+      nodeIntegration: true,
       contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js') // Opcional si tienes un archivo preload
     },
   });
-  win.webContents.on('before-input-event', (event, input) => {
-    if (input.key === 'F12') {
-      win.webContents.toggleDevTools();
-    }
-  });
-  win.loadFile('index.html');
+
+  loginWindow.loadFile('login.html');
 }
 
+function createWindowMain() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js') // Opcional si tienes un archivo preload
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+}
 
 // Conexión a la base de datos
 const connection = mysql.createConnection({
-  host: 'localhost', // Cambia esto si tu BD no está en localhost
-  user: 'root', // Usuario de la BD
-  password: '', // Contraseña de la BD
-  database: 'proyecto_deportologico', // Nombre de la BD
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'proyecto_deportologico',
 });
 
 connection.connect((err) => {
@@ -37,14 +52,13 @@ connection.connect((err) => {
 
 ipcMain.on('submit-registration', (event, formData) => {
   const { dni, nombre_usuario, password, nombre_regis, apellido_regis, email, fechaNacimiento, peso, altura, genero, objetivo_nutricional, dieta, obj_deportivo, tipo_deporte, frecuencia, intensidad } = formData;
-  // Validación de los datos
+
   let query = 'SELECT DNI, Nombre_usuario, Email FROM usuario WHERE DNI = ? OR Nombre_usuario = ? OR Email = ?';
   connection.query(query, [dni, nombre_usuario, email], (err, results) => {
     if (err) {
       console.error('Error checking data:', err);
       event.reply('registration-response', 'Error al verificar los datos');
     } else if (results.length > 0) {
-      // Verificar cuál campo ya existe
       if (results.some(row => row.DNI === dni)) {
         event.reply('registration-response', 'Error: El DNI ya está registrado');
       } else if (results.some(row => row.Nombre_usuario === nombre_usuario)) {
@@ -53,68 +67,80 @@ ipcMain.on('submit-registration', (event, formData) => {
         event.reply('registration-response', 'Error: El Email ya está registrado');
       }
     } else {
-
-      query = 'INSERT INTO `usuario`(`DNI`, `Nombre_usuario`, `password`, `Nombre`, `Apellido`, `FechaNacimiento`, `Email`, `Peso`, `Altura`, `Genero`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-      connection.query(query, [dni, nombre_usuario, password, nombre_regis, apellido_regis, fechaNacimiento, email, peso, altura, genero], (err, results) => {
+      // Hasheando la contraseña antes de insertar en la base de datos
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
         if (err) {
-          console.error('Error inserting data:', err);
-          event.reply('registration-response', 'Error al registrar en la sección de usuario');
-        } else {
-          console.log('Data inserted successfully in usuario:', results);
-
-          // Ahora insertar en nutricionalusuario
-          query = 'INSERT INTO `nutricionalusuario`(`ID_nut`, `DNI_nut`, `ObjetivoNutricion`, `TipoAlimentacion`) VALUES (?, ?, ?, ?)';
-          connection.query(query, ["", dni, objetivo_nutricional, dieta], (err, results) => {
-            if (err) {
-              console.error('Error inserting data in nutricionalusuario:', err);
-              event.reply('registration-response', 'Error al registrar en la sección de nutrición');
-            } else {
-              console.log('Data inserted successfully in nutricionalusuario:', results);
-
-              // Ahora insertar en deportivousuario
-              query = 'INSERT INTO `deportivousuario`(`ID_depor`, `DNI_depor`, `ObjetivosDeportivo`, `TipoDeporte`, `Frecuencia`, `Intensidad`) VALUES (?, ?, ?, ?, ?, ?)';
-              connection.query(query, ["", dni, obj_deportivo, tipo_deporte, frecuencia, intensidad], (err, results) => {
-                if (err) {
-                  console.error('Error inserting data in deportivousuario:', err);
-                  event.reply('registration-response', 'Error al registrar en la sección de deporte');
-                } else {
-                  console.log('Data inserted successfully in deportivousuario:', results);
-                  event.reply('registration-response', 'Registro exitoso en todas las secciones');
-                }
-              });
-            }
-          });
+          console.error('Error hashing password:', err);
+          event.reply('registration-response', 'Error al procesar la contraseña');
+          return;
         }
+
+        query = 'INSERT INTO `usuario`(`DNI`, `Nombre_usuario`, `password`, `Nombre`, `Apellido`, `FechaNacimiento`, `Email`, `Peso`, `Altura`, `Genero`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        connection.query(query, [dni, nombre_usuario, hashedPassword, nombre_regis, apellido_regis, fechaNacimiento, email, peso, altura, genero], (err, results) => {
+          if (err) {
+            console.error('Error inserting data:', err);
+            event.reply('registration-response', 'Error al registrar en la sección de usuario');
+          } else {
+            console.log('Data inserted successfully in usuario:', results);
+
+            query = 'INSERT INTO `nutricionalusuario`(`ID_nut`, `DNI_nut`, `ObjetivoNutricion`, `TipoAlimentacion`) VALUES (?, ?, ?, ?)';
+            connection.query(query, ["", dni, objetivo_nutricional, dieta], (err, results) => {
+              if (err) {
+                console.error('Error inserting data in nutricionalusuario:', err);
+                event.reply('registration-response', 'Error al registrar en la sección de nutrición');
+              } else {
+                console.log('Data inserted successfully in nutricionalusuario:', results);
+
+                query = 'INSERT INTO `deportivousuario`(`ID_depor`, `DNI_depor`, `ObjetivosDeportivo`, `TipoDeporte`, `Frecuencia`, `Intensidad`) VALUES (?, ?, ?, ?, ?, ?)';
+                connection.query(query, ["", dni, obj_deportivo, tipo_deporte, frecuencia, intensidad], (err, results) => {
+                  if (err) {
+                    console.error('Error inserting data in deportivousuario:', err);
+                    event.reply('registration-response', 'Error al registrar en la sección de deporte');
+                  } else {
+                    // Envía los datos al proceso de renderizado
+                    mainWindow.webContents.send('user-data', formData);
+
+                    // Cerrar la ventana de login y abrir la ventana principal
+                    if (loginWindow) {
+                      loginWindow.close();
+                      loginWindow = null;
+                    }
+                    createWindowMain();
+                  }
+                });
+              }
+            });
+          }
+        });
       });
     }
   });
 });
 
-
-
-
-
-const bcrypt = require('bcrypt');
-
-// En el evento de login
 ipcMain.on('submit-ini', (event, data_ini) => {
-  var { nombre_ini, email_ini, password_ini } = data_ini;
+  const { nombre_ini, email_ini, password_ini } = data_ini;
 
-  var query = 'SELECT Nombre_usuario, Email, password FROM usuario WHERE Nombre_usuario = ? OR Email = ?';
+  const query = 'SELECT Nombre_usuario, Email, password FROM usuario WHERE Nombre_usuario = ? OR Email = ?';
   connection.query(query, [nombre_ini, email_ini], (err, results) => {
     if (err) {
       console.error('Error checking data:', err);
       event.reply('login-response', 'Error al verificar los datos');
     } else if (results.length > 0) {
       const user = results[0];
-      
-      // Comparar la contraseña sin hashear con la contraseña hasheada en la base de datos
+
       bcrypt.compare(password_ini, user.password, (err, result) => {
         if (err) {
           console.error('Error comparing passwords:', err);
           event.reply('login-response', 'Error al verificar la contraseña');
         } else if (result) {
-          event.reply('login-response', 'success');
+          // Cerrar la ventana de login y abrir la ventana principal
+          if (loginWindow) {
+            loginWindow.close();
+            loginWindow = null;
+          }
+          createWindowMain();
+          // Envía los datos al proceso de renderizado
+          mainWindow.webContents.send('user-data_ini', data_ini);
         } else {
           event.reply('login-response', 'Error: Contraseña incorrecta');
         }
@@ -124,4 +150,5 @@ ipcMain.on('submit-ini', (event, data_ini) => {
     }
   });
 });
-app.whenReady().then(createWindow);
+
+app.whenReady().then(createWindowLogin);
